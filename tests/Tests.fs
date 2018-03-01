8cd378134ead9f62eea2a210dec92d51445ec46d
Håvard Sørbø <havard@hsorbo.no>
2018-03-01 18:00:42 -0500

    tests

---
+++ tests/Tests.fs
@@ -0,0 +1,15 @@
+module Tests
+open System
+open FsMisc
+open Xunit
+open Swensen.Unquote
+
+module SortedSeqDiffTests = 
+    open SortedSeqDiff
+    
+    [<Fact>]
+    let ``Generates correct diff`` () =
+        let left = [1;2;4;6;8;10]
+        let right = [1;3;4;5;6;11;12]
+        let result = [Both (1,1); Left(2); Right(3); Both(4,4); Right(5); Both(6,6); Left(8); Left(10); Right(11); Right(12)]
+        test <@ seqDiff id left right |> Seq.toList = result @>
