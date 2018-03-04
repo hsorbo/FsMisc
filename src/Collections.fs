82f4e2f68185168ef57e71fa0c9221620aad4923
Håvard Sørbø <havard@hsorbo.no>
2018-03-04 11:21:17 -0500

    Move into seqdiff into new file, create failing test

---
+++ src/Collections.fs
@@ -0,0 +1,30 @@
+namespace FsMisc
+
+module SortedSeqDiff =
+    type SeqDiff<'a> =
+    | Left of 'a
+    | Right of 'a
+    | Both of 'a * 'a
+
+    let private tryHeadAndTail s = s |> Seq.tryHead |> Option.map (fun h -> h, s |> Seq.skip 1)
+
+    let rec seqDiff chooser (left : seq<_>) (right : seq<_>) = seq {
+        let (l,r) = (left |> tryHeadAndTail, right |> tryHeadAndTail)
+        match (l,r) with
+        | (Some (lh, lt), Some (rh, _)) when chooser lh < chooser rh 
+            -> yield Left(lh)
+               yield! seqDiff chooser lt right
+        | (Some (lh, _), Some (rh, rt)) when chooser lh > chooser rh 
+            -> yield Right(rh)
+               yield! seqDiff chooser left rt
+        | (Some (lh, lt), Some (rh, rt))
+            -> yield Both(lh,rh)
+               yield! seqDiff chooser lt rt
+        | (Some (lh, lt), None)   
+            -> yield Left(lh)
+               yield! seqDiff chooser lt right
+        | (None, Some  (rh, rt))
+            -> yield Right(rh)
+               yield! seqDiff chooser left rt
+        | (None, None) -> ()
+    }
--- tests/Tests.fs
+++ tests/Tests.fs
@@ -13,3 +13,10 @@ module SortedSeqDiffTests =
         let right = [1;3;4;5;6;11;12]
         let result = [Both (1,1); Left(2); Right(3); Both(4,4); Right(5); Both(6,6); Left(8); Left(10); Right(11); Right(12)]
         test <@ seqDiff id left right |> Seq.toList = result @>
+
+    [<Fact>]
+    let ``Can handle large sequences`` () =
+        let s = System.Diagnostics.Stopwatch.StartNew()
+        printfn "Thinking"
+        let count = FsMisc.SortedSeqDiff.seqDiff id (seq { 0 .. 10000 }) (seq { 0 .. 10000 }) |> Seq.length
+        printfn "%A per ms" ((decimal count) / (decimal s.ElapsedMilliseconds))
